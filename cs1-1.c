abd230db44cd41bc8df6fa8f613933498f1b332e
ziqingw38 <68011978@kmitl.ac.th>
2025-07-08 04:35:06 +0000

    cs1-1

---
+++ cs1-1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("My name is: %s \n", "MR.PROGRAMMING");
+    printf("My Ipoint: %d\n", 20+30+39);
+    printf("Grade: %c\n",'A');
+    printf("GPA: %f", 3.99);
+    return(0);
+}
