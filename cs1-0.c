7054cb1e20deda44f8b2aec68ca8b266c6cbd5d9
ziqingw38 <68011978@kmitl.ac.th>
2025-07-08 04:30:53 +0000

    ex1-0

---
+++ cs1-0.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("%s","Hello World. \n");  //use '/n' to go to next line; '%s'= string
+    printf("Here is my first code.");
+    printf("I am a robotics and AI engineer");
+    return(0);
+}
