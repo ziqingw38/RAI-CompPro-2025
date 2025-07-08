aef477918fe3dde5a7ff16d11d1a5fa780d9ad9e
ziqingw38 <68011978@kmitl.ac.th>
2025-07-08 04:35:49 +0000

    cs1-2

---
+++ cs1-2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("Name=%s,ID=%s\n","PROGRAM", "64011014");
+    printf("Department: %s\n", "ROBOTICS AND AI");
+    printf("Age= %d, GPA=%.2f\n", 20,3.96);
+    printf("CProgramming: %.1f\n Robotics:%.2f\n", 4.0, 3.99);
+    return(0);
+}
